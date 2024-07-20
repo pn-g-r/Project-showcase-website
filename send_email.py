ddfeab4adb1707524bcf26660c0a0c497bcaeef0
ak-g-r <kistauri.ani@gmail.com>
2024-07-21 00:15:42 +0400

    Day 23: Added secure password

--- send_email.py
+++ send_email.py
@@ -1,17 +1,16 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "tt.test.159753159753@gmail.com"
-    password = "dumj kllh cpvc pnnd"
+    password = os.getenv("PASSWORD")
     receiver = "tt.test.159753159753@gmail.com"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
-
+#"dumj kllh cpvc pnnd"
